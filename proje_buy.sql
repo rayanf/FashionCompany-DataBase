116271322d9e75dceddd78829d4a94f14f47c825
rayanf <54745174+rayanf@users.noreply.github.com>
2021-09-04 18:29:54 +0430

    Update proje_buy.sql

--- proje_buy.sql
+++ proje_buy.sql
@@ -1,5 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
+--
 -- Host: localhost    Database: proje
 -- ------------------------------------------------------
 -- Server version	8.0.23
